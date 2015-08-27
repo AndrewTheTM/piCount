96467133274d29a73269a5b7ec93c12619774617
Andrew Rohne <arohne@oki.org>
2015-08-27 13:22:38 -0400

    image issues... checking

--- piCount.py
+++ piCount.py
@@ -23,11 +23,11 @@ class CamHandler(BaseHTTPRequestHandler):
                 stream = io.BytesIO()
                 camera = picamera.PiCamera()
                 camera.resolution = (640,480)
-                camera.start_preview()
                 time.sleep(2)
                 try:
 
                     img = camera.capture(stream,'jpeg')
+                    print img
                     data = numpy.fromstring(stream.getValue(), dtype=numpy.uint8)
                     print "got image?"
                     print data
