8b47db501babec3277ef97115c278ee7e7e1edc5
Andrew Rohne <arohne@oki.org>
2015-08-28 15:51:28 -0400

    tuning

--- piCount.py
+++ piCount.py
@@ -45,6 +45,7 @@ class CamHandler(BaseHTTPRequestHandler):
                         scaleFactor = 1.2,
                         minNeighbors = 8,
                         minSize = (50,30),
+                        maxSize = (120,100),
                         flags = cv2.CASCADE_SCALE_IMAGE)
                     for (x, y, w, h) in faces:
                         cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
