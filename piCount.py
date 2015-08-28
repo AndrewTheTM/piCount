719a43d0ed4ba256e0dc39bd668b73fde6f592ec
Andrew Rohne <arohne@oki.org>
2015-08-28 15:48:14 -0400

    tuning

--- piCount.py
+++ piCount.py
@@ -43,8 +43,8 @@ class CamHandler(BaseHTTPRequestHandler):
 
                     faces = faceCascade.detectMultiScale(fr2,
                         scaleFactor = 1.2,
-                        minNeighbors = 10,
-                        minSize = (30,30),
+                        minNeighbors = 8,
+                        minSize = (50,30),
                         flags = cv2.CASCADE_SCALE_IMAGE)
                     for (x, y, w, h) in faces:
                         cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
