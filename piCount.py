ec476ecc8721bd66b75406701d4175574e51bec1
Andrew Rohne <arohne@oki.org>
2015-09-02 13:12:45 -0400

    don't use this commit

--- piCount.py
+++ piCount.py
@@ -51,7 +51,7 @@ class CamHandler(BaseHTTPRequestHandler):
                     if saveImageMode:
                         f = "/home/pi/saveImage/img" + str(fn).zfill(4) + ".jpg"
                         # get 0,102 to 640,184
-                        cv2.imwrite(f,img[102:184,0:640])
+                        cv2.imwrite(f,img[102:286,0:640])
                         print "wrote image"
                         fn += 1
 
