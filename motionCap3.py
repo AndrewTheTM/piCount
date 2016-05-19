8288a545409af85586c6ede1a1d30d64f88f6b80
Andrew Rohne <arohne@oki.org>
2016-05-19 09:59:39 -0400

    Added threading to picamera

--- motionCap3.py
+++ motionCap3.py
@@ -159,10 +159,11 @@ class CamHandler(BaseHTTPRequestHandler):
 def main():
     global camera
     if RPI:
-        camera = picamera.PiCamera()
-        camera.resolution = (640,480)
-        camera.hflip = True
-        camera.vflip = True
+        # camera = picamera.PiCamera()
+        # camera.resolution = (640,480)
+        # camera.hflip = True
+        # camera.vflip = True
+        continue
     else:
         camera = cv2.VideoCapture(0)
         camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640);
