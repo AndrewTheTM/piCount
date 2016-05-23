539d51ff8a1d47153513f12adbcdacfac35de703
Andrew Rohne <arohne@oki.org>
2016-05-23 08:35:12 -0400

    increased kernel matrix to 10x10

--- motionCap3.py
+++ motionCap3.py
@@ -42,7 +42,7 @@ class CamHandler(BaseHTTPRequestHandler):
                     frame_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
                     mask = fgbg.apply(frame_gray)
-                    kernel = np.ones((5,5),np.float32)/25
+                    kernel = np.ones((10,10),np.float32)/1000
                     mask = cv2.dilate(mask, kernel, iterations = 2)
 
                     mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, None)
